caf86128ab89612dc59b08a7e2a702ceffddf36b
Abhay Pratap Singh <123452621+pundirabhay@users.noreply.github.com>
2023-02-02 13:53:35 +0530

    Add files via upload
    practice code

---
+++ main.py
@@ -0,0 +1,39 @@
+import turtle
+from turtle import Turtle, Screen
+import random
+is_race_on = False
+
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="make your bet", prompt="which turtle wins the race? enter your color:  ")
+colors  = ["red", "blue", "green", "yellow", "orange", "black"]
+y_postions = [-70, -40, -10, 20, 50, 80]
+all_turtle = []
+
+
+for turtle_index in range(6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x= -230, y= y_postions[turtle_index ])
+    all_turtle.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtle:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"you have won! the {winning_color} turtle as winner!")
+            else:
+                print(f"you have lost! the {winning_color} turtle as winner!")
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+
+
+screen.exitonclick()
